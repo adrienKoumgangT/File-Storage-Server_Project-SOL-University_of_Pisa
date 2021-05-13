4bb390bc38ec7e7868bf84ae0c0fc7fd52b3eb03
Adrien KOUMGANG TEGANTCHOUANG <adrientkoumgang@gmail.com>
2021-05-14 01:54:14 +0200

    file my_file.c created

---
+++ main/src/my_file.c
@@ -0,0 +1,15 @@
+/**
+ * @file my_file.c
+ *
+ * Definition of type file_t
+ *
+ * A generic file has :
+ * 		- a name (filename) : a string representing the key of the file
+ *		- a content (content) : the contents of the file
+ *		- a size of content (size) : the size of the content
+ *		- a pointer to another file (next) : to use to create a list of files
+ * 
+ * @author adrien koumgang tegantchouang
+ * @version 1.0
+ * @date 00/05/2021
+ */
