bfdb371b2eaf2f4bcde97e055eff8d59f8c0d455
Adrien KOUMGANG TEGANTCHOUANG <adrientkoumgang@gmail.com>
2021-05-14 01:52:50 +0200

    file my_hash.h created

---
+++ main/include/my_hash.h
@@ -0,0 +1,15 @@
+/**
+ * @file my_hash.h
+ *
+ * Definition of type hash_t
+ *
+ * A generic hash_t has :
+ * 		- a name (filename) : a string representing the key of the file
+ *		- a content (content) : the contents of the file
+ *		- a size of content (size) : the size of the content
+ *		- a pointer to another file (next) : to use to create a list of files
+ *
+ * @author adrien koumgang tegantchouang
+ * @version 1.0
+ * @date 00/05/2021
+ */
