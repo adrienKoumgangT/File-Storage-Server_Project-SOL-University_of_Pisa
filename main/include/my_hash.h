d513eca465b3bd4e129dd839c6cd1e8b05c8803e
Adrien KOUMGANG TEGANTCHOUANG <adrientkoumgang@gmail.com>
2021-05-14 20:09:05 +0200

    redefinition of the functions used for the hash table

--- main/include/my_hash.h
+++ main/include/my_hash.h
@@ -13,3 +13,49 @@
  * @version 1.0
  * @date 00/05/2021
  */
+
+
+ #if !defined(HASH_T)
+ #define HASH_T
+
+#include "my_file.h"
+
+ // definition of element to be inserted in the hash table
+ /*
+ typedef struct _file_t {
+   char *key;
+   char *data;
+   struct _file_t *next;
+ } file_t;
+ */
+ typedf struct _file_t data_hash_t;
+
+ /* the structure of hash table */
+ typedef struct _hash_t {
+ 	int size;
+ 	int number_of_item;
+ 	data_hash_t **table;
+ 	unsigned int (*hash_function)(char *);
+ 	int (*hash_key_compare)(char *, char *);
+ } hash_t;
+
+ /* Create a new hash table */
+ hash_t* hash_create( const int, unsigned int (*hash_function)(char *), int (*hash_key_compare)(char *, char *) );
+
+
+ void* hash_find( hash_t*, char* );
+
+
+ data_hash_t* hash_insert( hash_t*, data_hash_t* );
+
+
+ data_hash_t* hash_update_insert( hash_t*, data_hash_t* );
+
+
+ data_hash_t* hash_remove( hash_t*, char* );
+
+
+ int hash_destroy( hash_t* );
+
+
+ #endif
