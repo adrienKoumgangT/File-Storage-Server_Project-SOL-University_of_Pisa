6802cf54151b4a88d95cbf8046d1ee283cc9c51c
Adrien KOUMGANG TEGANTCHOUANG <adrientkoumgang@gmail.com>
2021-05-16 11:36:22 +0200

    update of the interface of the functions that work on the hash table

--- main/include/my_hash.h
+++ main/include/my_hash.h
@@ -1,3 +1,27 @@
+/*
+* MIT License
+*
+* Copyright (c) 2021 Adrien Koumgang Tegantchouang
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 /**
  * @file my_hash.h
  *
@@ -28,7 +52,7 @@
    struct _file_t *next;
  } file_t;
  */
- typedf struct _file_t data_hash_t;
+ typedef struct _file_t data_hash_t;
 
  /* the structure of hash table */
  typedef struct _hash_t {
@@ -40,19 +64,23 @@
  } hash_t;
 
  /* Create a new hash table */
- hash_t* hash_create( const int, unsigned int (*hash_function)(char *), int (*hash_key_compare)(char *, char *) );
+ hash_t* hash_create( const int, unsigned int (*hash_function)(char *),
+                        int (*hash_key_compare)(char *, char *) );
 
 
- void* hash_find( hash_t*, char* );
+ data_hash_t* hash_find( const hash_t*, char* );
 
 
- data_hash_t* hash_insert( hash_t*, data_hash_t* );
+ data_hash_t* hash_insert( hash_t*, char*, char* );
 
 
- data_hash_t* hash_update_insert( hash_t*, data_hash_t* );
+ data_hash_t* hash_update_insert( hash_t*, char*, char* );
 
 
  data_hash_t* hash_remove( hash_t*, char* );
+
+
+ int hash_delete( hash_t*, char* );
 
 
  int hash_destroy( hash_t* );
