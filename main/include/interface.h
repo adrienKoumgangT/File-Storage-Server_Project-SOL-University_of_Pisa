ed789879b1e53f0274044f2d382d08db07d3b955
Adrien KOUMGANG TEGANTCHOUANG <adrientkoumgang@gmail.com>
2021-05-17 15:20:50 +0200

    added of readNFile function in interface

--- main/include/interface.h
+++ main/include/interface.h
@@ -1,20 +1,58 @@
-#if !defined(INTERFACE_FOR_SERVER)
+/*
+* MIT License
+*
+* Copyright (c) 2021 Adrien Koumgang Tegantchouang
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+/**
+* @file interface.h
+*
+* definition of utility functions for client
+*
+* @author adrien koumgang tegantchouang
+* @version 1.0
+* @date 00/05/2021
+*/
+
+#ifndef INTERFACE_FOR_SERVER
 #define INTERFACE_FOR_SERVER
 
-int closeConnection(const char* sockname);
+int openConnection( const char* sockname, int msec, const struct timespec abstime );
 
-int openFile(const char* pathname, int flags);
+int closeConnection( const char* sockname );
 
-int readFile(const char* pathname, void** buf, size_t* size);
+int openFile( const char* pathname, int flags );
 
-int appendToFile(const char* pathname, void* buf, size_t size, const char* dirname);
+int readFile( const char* pathname, void** buf, size_t* size );
 
-int lockFile(const char* pathname);
+int readNFile( int N, const char* dirname );
 
-int unlockFile(const char* pathname);
+int appendToFile( const char* pathname, void* buf, size_t size, const char* dirname );
 
-int closeFile(const char* pathname);
+int lockFile( const char* pathname );
 
-int removeFile(const char* pathname);
+int unlockFile( const char* pathname );
+
+int closeFile( const char* pathname );
+
+int removeFile( const char* pathname );
 
 #endif
