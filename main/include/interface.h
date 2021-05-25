5366aa71850263d65c96ae1d3299b7ad099d44f6
Adrien KOUMGANG TEGANTCHOUANG <adrientkoumgang@gmail.com>
2021-05-26 00:26:41 +0200

    added of writeFile interface

--- main/include/interface.h
+++ main/include/interface.h
@@ -45,6 +45,8 @@ int readFile( const char* pathname, void** buf, size_t* size );
 
 int readNFile( int N, const char* dirname );
 
+int writeFile( const char* pathname, const char* dirname );
+
 int appendToFile( const char* pathname, void* buf, size_t size, const char* dirname );
 
 int lockFile( const char* pathname );
