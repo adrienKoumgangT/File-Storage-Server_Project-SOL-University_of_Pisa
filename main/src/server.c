e61bb22f79721f8b43791eff8aac4eafe1214e18
Adrien KOUMGANG TEGANTCHOUANG <adrientkoumgang@gmail.com>
2021-05-29 01:48:15 +0200

    definition of the server master function

--- main/src/server.c
+++ main/src/server.c
@@ -39,46 +39,118 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <time.h>
+#include <ctype.h>
 #include <errno.h>
 #include <limits.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <sys/uio.h>
 #include <sys/un.h>
 
 #include <assert.h>
 
-#include "server.h"
+#include "communication.h"
 #include "utils.h"
-
+#include "my_hash.h"
+#include "my_file.h"
+#include "queue.h"
 
 // define for all programs
 #define STR_LEN 80
 #define STR_SIZE (STR_LEN * sizeof(char))
+#define DIM_HASH_TABLE 103
 
-// variabile globale per il dbg
-int tempo_dgb = 1;
+// define for config server
+#define n_param_config 5
+#define t_w "THREAD_WORKERS"
+#define s_m "SIZE_MEMORY"
+#define n_f "NUMBER_OF_FILES"
+#define s_n "SOCKET_NAME"
+#define c_c "CONCURRENT_CLIENTS"
 
-// server variables
-struct _config_for_server configurations_server;
-struct _hash_t *files_server;
+static int info = 1;
+static unsigned long tempo_dgb = 1;
 
+static int close_server = 0;
 
-/* ###############################################
-*  #definition of server configuration functions #
-*  ###############################################
+/**
+* Definition of the structure that will have the server configuration data
 */
+typedef struct _config_for_server{
+    unsigned long thread_workers;
+    unsigned long concurrent_clients;
+    unsigned long size_memory;
+    unsigned long number_of_files;
+    char *socket_name;
+}cfs;
+
+
+
+/*************** server variables *****************/
+
+// variable containing server configurations
+static cfs settings_server;
+
+// file containers
+static hash_t *files_server;
+
+// request buffer
+static Queue_t* buffer_request;
+
+static int canale[2];
+
+/*********** structure for counting elements in mutual exclusion **********/
+
+typedef struct _count_elem{
+    long count;
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+} count_elem_t;
+
+
+/************************* number of threads created *************************/
+static count_elem_t* threads_created;
+
+/************************* number of client conneced *************************/
+static count_elem_t* clients_connected;
+
+/******************* pipe for communication Workers -> Master ****************/
+
+
+
+/********* cleanup function ****/
+
+void cleanup( void ){
+    unlink(settings_server.socket_name);
+}
+
+/*********** function to initialised the structure for counting elements in mutual exclusion **********/
+
+count_elem_t* init_struct_count_elem( void ){
+    int err;
+    count_elem_t* t;
+    SYSCALL_RETURN_EQ("malloc", t, (count_elem_t *) malloc(sizeof(count_elem_t)), NULL, "");
+    t->count = 0;
+    SYSCALL_RETURN_VAL_NEQ("pthread_mutex_init", err, pthread_mutex_init(&t->lock, NULL), 0, NULL, "");
+    SYSCALL_RETURN_VAL_NEQ("pthread_cond_init", err, pthread_cond_init(&t->cond, NULL), 0, NULL, "");
+    return t;
+}
+
+/*************** definition of server configuration functions ***************/
+
 
 /**
 * reinitializes the server configurations
 */
 void reset_cfs(){
-    cfs *config = &configurations_server;
+    cfs *config = &settings_server;
 
     if(!config)
         exit(EXIT_FAILURE);
 
     config->thread_workers = 0;
+    config->concurrent_clients = 0;
     config->size_memory = 0;
     if(config->socket_name)
         free(config->socket_name);
@@ -92,15 +164,18 @@ void reset_cfs(){
 * are well done for it to work
 *
 * @returns : -1 if server has bad configuration
-*              1 if is good configuration server
+*              0 if is good configuration server
 */
 int is_correct_cfs(){
-    cfs *config = &configurations_server;
+    cfs *config = &settings_server;
 
     if(!config)
         exit(EXIT_FAILURE);
 
     if(config->thread_workers <= 1)
+        return -1;
+
+    if(config->concurrent_clients <= 1)
         return -1;
 
     if(config->size_memory <= 0)
@@ -112,7 +187,7 @@ int is_correct_cfs(){
     if(config->socket_name == NULL)
         return -1;
 
-    return 1;
+    return 0;
 }
 
 
@@ -121,13 +196,15 @@ int is_correct_cfs(){
 */
 void read_config_server(){
 
-fprintf(stdout, "%d : entro dentro read_config_server\n", tempo_dgb++);
+if(info) fprintf(stdout, "=>%ld<= : reading server configuration in progress...\n", tempo_dgb++);
 
-    cfs *config = &configurations_server;
+    cfs *config = &settings_server;
 
     fprintf(stdout, "information about the current server configuration :\n");
     fprintf(stdout, "number of threads workers = %ld\n",
                         config->thread_workers);
+    fprintf(stdout, "number of concurrent clients = %ld\n",
+                        config->concurrent_clients);
     fprintf(stdout, "size of memory for server = %ld\n",
                         config->size_memory);
     fprintf(stdout, "max number of files to write on server = %ld\n",
@@ -136,7 +213,7 @@ fprintf(stdout, "%d : entro dentro read_config_server\n", tempo_dgb++);
                         config->socket_name);
     fflush(stdout);
 
-fprintf(stdout, "%d : esco da read_config_server\n", tempo_dgb++);
+if(info) fprintf(stdout, "=>%ld<= : finished server configuration reading.\n", tempo_dgb++);
 
 }
 
@@ -152,10 +229,7 @@ fprintf(stdout, "%d : esco da read_config_server\n", tempo_dgb++);
 **/
 int parsing_str_for_config_server( char* str ){
 
-fprintf(stdout, "%d : entro dentro parsing_str_for_config_server\n",
-                                tempo_dgb++);
-
-    cfs *config = &configurations_server;
+    cfs *config = &settings_server;
 
     if(!str || !config)
         return -1;
@@ -171,19 +245,28 @@ fprintf(stdout, "%d : entro dentro parsing_str_for_config_server\n",
 
             if( (config->thread_workers = (unsigned long) getNumber(token, 10)) < 0)
                 return -1;
+
+        }else if(strncmp(token, c_c, sizeof(c_c)) == 0){
+            token = strtok_r(NULL, ":", &tmp);
+            token[strcspn(token, "\n")] = '\0';
+
+            if( (config->concurrent_clients = (unsigned long) getNumber(token, 10)) < 0)
+                return -1;
+
         }else if(strncmp(token, s_m, sizeof(s_m)) == 0){
             token = strtok_r(NULL, ":", &tmp);
             token[strcspn(token, "\n")] = '\0';
 
             if( (config->size_memory = (unsigned long) getNumber(token, 10)) < 0)
                 return -1;
+
         }else if(strncmp(token, n_f, sizeof(n_f)) == 0){
             token = strtok_r(NULL, ":", &tmp);
             token[strcspn(token, "\n")] = '\0';
 
-            if( (config->number_of_files = (unsigned long) getNumber(token, 10))
-                                    < 0)
+            if( (config->number_of_files = (unsigned long) getNumber(token, 10)) < 0)
                 return -1;
+
         }else if(strncmp(token, s_n, sizeof(s_n)) == 0){
             token = strtok_r(NULL, ":", &tmp);
             token[strcspn(token, "\n")] = '\0';
@@ -203,8 +286,6 @@ fprintf(stdout, "%d : entro dentro parsing_str_for_config_server\n",
         token = strtok_r(NULL, ":", &tmp);
     }
 
-fprintf(stdout, "%d : esco da parsing_str_for_config_server\n", tempo_dgb++);
-
     return 0;
 }
 
@@ -219,9 +300,6 @@ fprintf(stdout, "%d : esco da parsing_str_for_config_server\n", tempo_dgb++);
 *            - -1 on error
 */
 int config_file_parser_for_server( FILE* file ){
-
-fprintf(stdout, "%d : entro dentro config_file_parser_for_server\n",
-                                tempo_dgb++);
 
     int err;
 
@@ -254,8 +332,6 @@ fprintf(stdout, "%d : entro dentro config_file_parser_for_server\n",
 
     SYSCALL_RETURN_EQ("is_correct_cfs", err, is_correct_cfs(), -1, "Failure to configure server");
 
-fprintf(stdout, "%d : esco da config_file_parser_for_server\n", tempo_dgb++);
-
     return 0;
 
 }
@@ -270,7 +346,7 @@ fprintf(stdout, "%d : esco da config_file_parser_for_server\n", tempo_dgb++);
 */
 int config_server( char* path_file_config ){
 
-fprintf(stdout, "%d : entro dentro config_server\n", tempo_dgb++);
+if(info) fprintf(stdout, "=>%ld<= : reading server configurations from configuration file in progress...\n", tempo_dgb++);
 
     int err;
     FILE *file_config;
@@ -282,22 +358,173 @@ fprintf(stdout, "%d : entro dentro config_server\n", tempo_dgb++);
 
     read_config_server();
 
-fprintf(stdout, "%d : esco da config_server\n", tempo_dgb++);
+fprintf(stdout, "=>%ld<= : reading server configurations from finished configuration file.\n", tempo_dgb++);
 
     return 0;
 }
 
-/**
-* main function of the server that starts the server
-*/
-void run_server( char* path_file_config ){
+/****************************** thread workers *******************************/
 
-fprintf(stdout, "%d : entro dentro run_server\n", tempo_dgb++);
+void* workers( void* args ){
+
+    
+    return NULL;
+}
+
+/******************************* thread master *******************************/
+
+/**
+* master: function of the server that starts the server
+*/
+void master( void ){
+    int i, err;
+
+if(info) fprintf(stdout, "=>%ld<= : creating a file descriptor container.\n", tempo_dgb++);
+
+    fd_set set, tmpset;
+    FD_ZERO(&set);
+    FD_ZERO(&tmpset);
+
+if(info) fprintf(stdout, "=>%ld<= : creation and configuration of the server communication channel with clients in progress...\n", tempo_dgb++);
+
+    struct sockaddr_un server_addr;
+    memset(&server_addr, '0', sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, settings_server.socket_name, strlen(settings_server.socket_name)+1);
+
+    int fd_socket = -1;
+    SYSCALL_EXIT_EQ("socket", fd_socket, socket(AF_UNIX, SOCK_STREAM, 0), -1, "");
+    printf("ciao mondo!!!\n");
+    SYSCALL_EXIT_EQ("bind", err, bind(fd_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)), -1, "");
+    printf("salut le monde!!!\n");
+    SYSCALL_EXIT_EQ("listen", err, listen(fd_socket, settings_server.concurrent_clients), -1, "");
+
+    FD_SET(fd_socket, &set);
+
+if(info) fprintf(stdout, "=>%ld<= : finished creation and configuration of the server communication channel with clients.\n",tempo_dgb++);
+
+if(info) fprintf(stdout, "=>%ld<= : configuration of the methods of creation and functioning of threads in progress...\n", tempo_dgb++);
+
+    SYSCALL_EXIT_EQ("pipe", err, pipe(canale), -1, "");
+    FD_SET(canale[0], &set);
+
+    pthread_attr_t thread_attr;
+    pthread_t thread_id;
+
+    SYSCALL_EXIT_NEQ("pthread_attr_init", err, pthread_attr_init(&thread_attr), 0, "");
+    SYSCALL_EXIT_NEQ("pthread_attr_setdetachstate", err, pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED), 0, "");
+
+    /*
+    thread_id = (pthread_t *) malloc(settings_server.thread_workers * sizeof(pthread_t));
+    for(i=0; i<settings_server.thread_workers; i++)
+        thread_id[i] = -1;
+    */
+
+    SYSCALL_EXIT_EQ("init_struct_count_elem", threads_created, init_struct_count_elem(), NULL, "");
+
+if(info) fprintf(stdout, "=>%ld<= : configuration of the methods of creation and functioning of the finished threads.\n", tempo_dgb++);
+
+if(info) fprintf(stdout, "=>%ld<= : beginning of acceptance of requests.\n", tempo_dgb++);
+
+    SYSCALL_EXIT_EQ("init_struct_count_elem", clients_connected, init_struct_count_elem(), NULL, "");
+
+    SYSCALL_EXIT_EQ("hash_create", files_server, hash_create( DIM_HASH_TABLE, &hash_function_for_file_t, &hash_key_compare_for_file_t ) , NULL, "")
+
+    SYSCALL_EXIT_EQ("initQueue", buffer_request, initQueue(), NULL, "");
+
+    int fdmax = canale[0];
+
+    do{
+        tmpset = set;
+        SYSCALL_EXIT_EQ("select", err, select(fdmax+1, &tmpset, NULL, NULL, NULL), -1, "");
+
+        // let's try to understand from which file descriptor we received a request
+        for(i=0; i<= fdmax; i++){
+            if(FD_ISSET(i, &tmpset)){
+                long connfd = -1;
+                // if it is a new connection request
+                if(i == fd_socket){
+                    // check if I have reached the limit number of clients connected at the same time
+                    pthread_mutex_lock(&clients_connected->lock);
+                    if(clients_connected->count >= settings_server.concurrent_clients){
+                        pthread_mutex_unlock(&clients_connected->lock);
+                        continue;
+                    }else{
+                        int c1 = ++clients_connected->count;
+                        pthread_mutex_unlock(&clients_connected->lock);
+                        SYSCALL_EXIT_EQ("accept", connfd, accept(fd_socket, (struct sockaddr *)NULL, NULL), -1, "");
+                        FD_SET(connfd, &set);
+                        if(connfd > fdmax) fdmax = connfd;
+                        pthread_mutex_lock(&threads_created->lock);
+                        if(threads_created->count < (c1 + 3)){
+                            if(pthread_create(&thread_id, &thread_attr, workers, (void *) NULL) != 0){
+                                fprintf(stderr, "pthread_create FAILED\n");
+                            }
+                        }
+                        pthread_mutex_unlock(&threads_created->lock);
+                        continue;
+                    }
+                }
+
+                // if it is the worker thread who has finished handling a client request
+                if(i == canale[0]){
+                    char toClose[2];
+                    memset(toClose, '\0', 2);
+                    SYSCALL_EXIT_EQ("read", err, read(canale[0], &connfd, sizeof(long)), -1, "");
+                    SYSCALL_EXIT_EQ("read", err, read(canale[0], toClose, 2), -1, "");
+                    if(strncmp(toClose, "N", 2) == 0){
+                        FD_SET(connfd, &set);
+                        if(connfd > fdmax) fdmax = connfd;
+                    }
+                    continue;
+                }
+
+                // if it is a generic request from a client
+                connfd = i;
+                // int* fd_client_request;
+                push(buffer_request, (void *) connfd);
+            }
+        }
+
+    }while(!close_server);
+
+    pthread_mutex_lock(&threads_created->lock);
+    while(threads_created->count > 0){
+        pthread_cond_wait(&threads_created->cond, &threads_created->lock);
+    }
+    pthread_mutex_lock(&threads_created->lock);
+
+    close(canale[0]);
+    close(canale[1]);
+
+    SYSCALL_EXIT_NEQ("pthread_attr_destroy", err, pthread_attr_destroy(&thread_attr), 0, "");
+    free(threads_created);
+    free(clients_connected);
+    SYSCALL_EXIT_EQ("hash_destroy", err, hash_destroy(files_server), -1, "");
+    //SYSCALL_EXIT_EQ("deleteQueue", err, deleteQueue(buffer_request), void, "");
+    deleteQueue(buffer_request);
+}
+
+/******************************* main function *******************************/
+
+int main(int argc, char** argv){
+//    cleanup();
+//    atexit(cleanup);
+
+    if(argc < 2){
+        fprintf(stderr, "test file execution scheme: %s pathname_file_config\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     int err;
 
-    SYSCALL_EXIT_EQ("config_server", err, config_server(path_file_config), -1,
-                            "server configuration");
+    SYSCALL_EXIT_EQ("config_server", err, config_server(argv[1]), -1,
+                            "server configuration failure");
 
+if(info) fprintf(stdout, "=>%ld<= : server startup!\n", tempo_dgb++);
 
-fprintf(stdout, "%d : esco da run_server\n", tempo_dgb++);
+    master();
+
+if(info) fprintf(stdout, "=>%ld<= : server closed!\n", tempo_dgb++);
+    return 0;
 }
